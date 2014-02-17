001f9e6f309a8fd660c299bcca1a209433de2254
Shiv Prakash <watsindename@gmail.com>
2014-02-16 20:36:22 -0800

    Grammer

--- compile-nix.sh
+++ compile-nix.sh
@@ -13,14 +13,14 @@ javah -jni NativeMethods
 echo "Header file created."
 
 # May need to set this varible if you get "Exception in thread "main" java.lang.UnsatisfiedLinkError"
-# Set it to path where the .so is located 
+# Set it to path where the .so is located.
 export LD_LIBRARY_PATH=.
 
 # Options
 # -I provide path to JAVA_HOME/include and JAVA_HOME/include/linux directories. I have hardcoded this path, verify this path for your own system. 
 #    Make sure the include direcory contains the files jni.h & jni_md.h
-# -o filename should be prefixed with 'lib' and extension should be .so for *nix os's
-# -fPIC interesting read @ http://stackoverflow.com/questions/5311515/gcc-fpic-option
+# -o filename should be prefixed with 'lib' and extension should be .so for *nix os's.
+# -fPIC interesting read @ http://stackoverflow.com/questions/5311515/gcc-fpic-option.
 gcc -fPIC -I/usr/lib/jvm/java-1.7.0-openjdk-amd64/include -I/usr/lib/jvm/java-1.7.0-openjdk-amd64/include/linux -shared cImplOne.c cImplTwo.c -o libNativeLib.so
 echo "Shared library created."
 echo "-------------------------------"
